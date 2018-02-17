ea12a8f5a69ab4bca7b9ac58b1723a2861b374ac
RoyalKingMomo <malahdal@ualberta.ca>
2018-02-17 15:40:23 -0700

    temporary path

--- particlerun.tech frontend/map.js
+++ particlerun.tech frontend/map.js
@@ -186,4 +186,23 @@ function initMap() {
   }
 ]
     });
+
+    var currentPathTaken = [
+        {
+            lat: 37.772,
+            lng: -122.214
+        },
+        {
+            lat: 21.291,
+            lng: -157.821
+        },
+        {
+            lat: -18.142,
+            lng: 178.431
+        },
+        {
+            lat: -27.467,
+            lng: 153.027
+        }
+        ];
 }
