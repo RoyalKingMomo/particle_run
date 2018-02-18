b4dcbaf7fb3e37af763c6339295834d4ba8b48d9
RoyalKingMomo <malahdal@ualberta.ca>
2018-02-18 01:02:34 -0700

    Revert "Merge branch 'master' of https://github.com/CalgaryHacks2018/particle_run"
    This reverts commit 7776125794a449972621685fe5e82f0795f53cd5, reversing
    changes made to 6b6891958ab8117e975fac429984f1cafbc34644.

--- app.js
+++ app.js
@@ -1,36 +1,21 @@
-var express        = require("express"),
-    cookieParser   = require("cookie-parser"),
-    session        = require("express-session"),
-    app            = express(),
-    bodyParser     = require("body-parser"),
-    mongoose       = require("mongoose"),
-    flash          = require("connect-flash"),
-    passport       = require("passport"),
-    LocalStrategy  = require("passport-local"),
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
     methodOverride = require("method-override"),
-    MongoStore     = require("connect-mongo")(session),
     User           = require("./models/user");
 
 var indexRoutes = require("./routes/index"),
     userRoutes = require("./routes/user");
 
-mongoose.connect("mongodb://localhost/particlerun", {useMongoClient: true});
+mongoose.connect("mongodb://localhost/particlerun");
 mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
-app.use(flash());
-// seedDB();
-
-app.use(cookieParser());
-app.use(require("express-session")({
-    secret: "QWER1234",
-    resave: false,
-    saveUninitialized: false,
-    store: new MongoStore({mongooseConnection: mongoose.connection}),
-    cookie: {maxAge: 180 * 60 * 1000} //min * sec * millisec
-}));
 
 // Passport Configuration
 app.use(passport.initialize());
@@ -41,9 +26,6 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
-    res.locals.session = req.session;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
     next();
 });
 
